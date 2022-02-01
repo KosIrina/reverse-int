dbc27e5e2121d058d0ba95e44b220a0113782af9
Irina Kosyanchuk <kosyanchuk.ira@gmail.com>
2022-02-01 09:29:34 +0200

    add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+
+    let number = Math.abs(n);
+    let revertedString = number.toString().split('').reverse().join('');
+    let revertedNumber = Number(revertedString);
+      
+    return revertedNumber;
+
 }
